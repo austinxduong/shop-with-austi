e6b1dd286dd9effbe5cf26b619924baf942acb2f
austinxduong <austinxduong@gmail.com>
2025-09-15 20:00:30 -0700

    async function for item summary

--- server/seed-database.ts
+++ server/seed-database.ts
@@ -85,7 +85,7 @@ async function createVectorSearchIndex(): Promise<void> {
     }
 }
 
-async function generateSyntheticData(): Promise <void> {
+async function generateSyntheticData(): Promise <Item[]> {
     const prompt = `You are a helpful assistant that generates furniture 
     store item data. Generate 10 furniture store items. Each record should
     include the following fields: item_id, item_name, item_description,
@@ -97,4 +97,27 @@ async function generateSyntheticData(): Promise <void> {
     console.log("Generating synthetic data...")
 
     const response = await llm.invoke(prompt)
+
+    return parser.parse(response.content as string)
+}
+
+async function createItemSummary(item: Item) : Promise<string> {
+    return new Promise((resolve) => {
+        const manufacturerDetails = `Made in ${item.manufacturer_address.country}`
+        const categories = item.categories.join(",")
+        const userReviews = item.user_reviews.map((review) => 
+            `Rated ${review.rating} on ${review.review_date}: ${review.comment}`)
+        .join(" ")
+        const basicInfo = `${item.item_name} ${item.item_description}
+        from the brand ${item.brand}`
+        const price = `At full price it costs: ${item.prices.full_price} USD,
+        On sale it costs: ${item.prices.sale_price} USD`
+        const notes = item.notes
+
+        const summary = `${basicInfo}. Manufacturer: ${manufacturerDetails}.
+        Categories: ${categories}. Reviews: ${userReviews}. Price: ${price}.
+        Notes: ${notes}`
+
+        resolve(summary)
+        })
 }
